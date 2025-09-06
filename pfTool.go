8229818392c0984d30b0a85def7d76853c594edb
Roman <bobbaz@debra.bobbazLab>
2025-09-06 14:39:37 +0100

    unmarshal json

--- pfTool.go
+++ pfTool.go
@@ -1,6 +1,64 @@
 package main
-import ("fmt")
+import (
+     "os"
+     "fmt"
+     "encoding/json"
+     "reflect"
+
+)
+
+const defCommand string = "ssh"
+
+var baseArgs = []string{
+    "-N",
+    "-o",
+    "ExitOnForwardFaliure=yes",
+    "-o",
+    "StreamLocalBindUnlink=yes",
+    "-o",
+    "ServerAliveInterval=5",
+    "-o",
+    "ServerAliveCountMax=1",
+    "-L",
+
+
+}
+
+type configStruct struct {
+	Host string `json:"host"`
+	Type string `json:"Type"`
+	Port int `json:"Port"`
+}
 
 func main(){
-  fmt.Println("Hello")
+	data, err := os.ReadFile("./pfToolConfig.json")
+	if err != nil {
+		fmt.Printf("Error Reading Config file", err)
+		os.Exit(1)
+	}
+	
+	var cfg configStruct
+
+	err =json.Unmarshal(data, &cfg)
+	if err != nil {
+		fmt.Printf("Error Unmarshlling Json", err)
+		os.Exit(1)
+	}
+
+	result := make(map[string][]string)
+
+	mapValue := reflect.ValueOf(cfg)
+	mapType := reflect.TypeOf(cfg)
+
+	fields := []string{}
+	for i := 0; i< mapValue.NumField(); i++{
+		fieldValue := mapValue.Field(i).Interface()
+		fields = append(fields, fmt.Sprintf("%v", fieldValue))
+	}
+
+	result[mapType.Name()] = fields
+	
+	fmt.Println(result)
+
+	
 }
