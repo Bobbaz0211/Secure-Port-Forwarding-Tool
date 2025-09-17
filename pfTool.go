3b5c9eaa77c410c1575002a16feadb1e5ebeca39
Roman <romanh0211@gmail.com>
2025-09-17 20:20:31 +0100

    Setup and process functions

--- pfTool.go
+++ pfTool.go
@@ -1,64 +1,134 @@
 package main
-import (
-     "os"
-     "fmt"
-     "encoding/json"
-     "reflect"
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"time"
 )
 
 const defCommand string = "ssh"
 
 var baseArgs = []string{
-    "-N",
-    "-o",
-    "ExitOnForwardFaliure=yes",
-    "-o",
-    "StreamLocalBindUnlink=yes",
-    "-o",
-    "ServerAliveInterval=5",
-    "-o",
-    "ServerAliveCountMax=1",
-    "-L",
-
-
+	"-N",
+	"-o",
+	"ExitOnForwardFaliure=yes",
+	"-o",
+	"StreamLocalBindUnlink=yes",
+	"-o",
+	"ServerAliveInterval=5",
+	"-o",
+	"ServerAliveCountMax=1",
+	"-L",
 }
 
-type configStruct struct {
-	Host string `json:"host"`
-	Type string `json:"Type"`
-	Port int `json:"Port"`
+func setupExec(target string, metadata []string) (*exec.Cmd, error) {
+	var cmd *exec.Cmd
+	var cmdErr error
+	var cmdArgs []string = nil
+
+	switch metadata[1] {
+	case "tcp":
+		cmdArgs = append(baseArgs, fmt.Sprintf("%s:localhost:%s",
+			metadata[0], metadata[0]))
+
+	default:
+		fmt.Println(metadata[1])
+		return nil, cmdErr
+
+	}
+
+	cmdArgs = append(cmdArgs, metadata[2])
+	cmd = exec.Command(defCommand, cmdArgs...)
+	cmdErr = cmd.Start()
+
+	if cmdErr != nil {
+		return nil, cmdErr
+	}
+
+	return cmd, cmdErr
 }
 
-func main(){
+func processViewer(target string, metadata []string) {
+
+	var cmd *exec.Cmd
+	var cmdErr error
+
+	for true {
+		//exec and start process
+		// wait for exit
+		//sleep 5s
+
+		cmd, cmdErr = setupExec(target, metadata)
+
+		if cmd == nil || cmdErr != nil {
+			fmt.Println("Cannot spawn process\n")
+
+			time.Sleep(time.Duration(time.Duration(5) * time.Second))
+			continue
+		}
+
+		fmt.Println("Spawned process for '%s' as %d\n")
+
+		cmd.Wait()
+
+		fmt.Println("Process for '%s' died, Waiting for %ds before restarting...\n")
+
+		time.Sleep(time.Duration(5) * time.Second)
+
+	}
+}
+
+func main() {
+
+	var cfgContainer map[string]interface{}
+	var cfg = map[string][]string{}
+	var start string
+	var first bool = false
+
 	data, err := os.ReadFile("./pfToolConfig.json")
 	if err != nil {
-		fmt.Printf("Error Reading Config file", err)
+		fmt.Printf("Error Reading Config file!\n")
 		os.Exit(1)
 	}
-	
-	var cfg configStruct
 
-	err =json.Unmarshal(data, &cfg)
+	err = json.Unmarshal(data, &cfgContainer)
 	if err != nil {
-		fmt.Printf("Error Unmarshlling Json", err)
+		fmt.Printf("Error Unmarshlling Json\n")
 		os.Exit(1)
 	}
 
-	result := make(map[string][]string)
+	for i := range cfgContainer {
 
-	mapValue := reflect.ValueOf(cfg)
-	mapType := reflect.TypeOf(cfg)
+		switch interfaceType := cfgContainer[i].(type) {
 
-	fields := []string{}
-	for i := 0; i< mapValue.NumField(); i++{
-		fieldValue := mapValue.Field(i).Interface()
-		fields = append(fields, fmt.Sprintf("%v", fieldValue))
+		case []interface{}:
+
+			for j := 0; j < len(interfaceType); j++ {
+
+				switch valueType := interfaceType[j].(type) {
+
+				case string:
+					cfg[i] = append(cfg[i], valueType)
+				default:
+					continue
+				}
+			}
+		default:
+			continue
+		}
 	}
 
-	result[mapType.Name()] = fields
-	
-	fmt.Println(result)
+	for i := range cfg {
+		if !first {
+			first = true
+			start = i
+			continue
+		}
+		go processViewer(i, cfg[i])
+	}
 
-	
+	processViewer(start, cfg[start])
+
 }
